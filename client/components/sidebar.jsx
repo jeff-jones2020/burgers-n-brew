00a680d4f6f09eac5383da9611dfcd786c0ffd91
Lawrence <lawrey7@gmail.com>
2020-03-17 16:36:41 -0700

    Added a SideBar component.

---
+++ client/components/sidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+class SideBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="location">
+
+        </div>
+        <div className="account">
+
+        </div>
+        <div className="price-filter">
+
+        </div>
+        <div className="proximity-filter">
+
+        </div>
+        <div className="vegetarian-filter">
+
+        </div>
+        <div className="ratings">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
