cf7b523b23681ebb63c187d90ef1701beb0341e8
Lawrence <lawrey7@gmail.com>
2020-03-17 17:05:27 -0700

    Added sidebar icon.

--- client/components/sidebar.jsx
+++ client/components/sidebar.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 class SideBar extends React.Component {
   render() {
-    return (
-      <div>
-        <div className="location">
-          <div className="location-icon" />
-        </div>
-        <div className="account">
-          <div className="account-icon" />
-        </div>
-        <div className="price-filter">
-          <div className="filter-icon" />
-        </div>
-        <div className="proximity-filter">
-          <div className="filter-icon" />
-        </div>
-        <div className="vegetarian-filter">
-          <div className="filter-icon" />
-        </div>
-        <div className="ratings">
+    const open = this.props.opened;
 
+    if (open) {
+      return (
+        <div>
+          <div className="location">
+            <div className="location-icon" />
+          </div>
+          <div className="account">
+            <div className="account-icon" />
+          </div>
+          <div className="price-filter">
+            <div className="filter-icon" />
+          </div>
+          <div className="proximity-filter">
+            <div className="filter-icon" />
+          </div>
+          <div className="vegetarian-filter">
+            <div className="filter-icon" />
+          </div>
+          <div className="ratings">
+
+          </div>
         </div>
-      </div>
+      );
+    }
+    return (
+      <div className="sidebar" />
     );
   }
 }
