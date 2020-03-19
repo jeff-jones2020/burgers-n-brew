9e8cdc411925c4950d2e13d69f8012e72ac62d77
Lawrence <lawrey7@gmail.com>
2020-03-19 14:46:51 -0700

    Finished functionality for the sidebar component.

--- client/components/sidebar.jsx
+++ client/components/sidebar.jsx
@@ -9,38 +9,31 @@ class SideBar extends React.Component {
     return (
       <>
         <div className="sidebar-icon" onClick={this.props.openSideBar} />
-        <CSSTransition
-          in={open}
-          classNames="appear"
-          unmountOnExit
-          timeout={500}
-        >
-          <div className="sidebar" onClick={this.props.openSideBar}>
-            <CSSTransition
-              in={open}
-              classNames="slide-in"
-              unmountOnExit
-              timeout={500}>
-              <div className="sidebar-container">
-                <div className="location">
-                  <DropDown />
-                </div>
-                <div className="account">
-                  <div className="account-icon" />
-                </div>
-                <div className="price-filter">
-                  <div className="filter-icon" />
-                </div>
-                <div className="proximity-filter">
-                  <div className="filter-icon" />
-                </div>
-                <div className="vegetarian-filter">
-                  <div className="filter-icon" />
-                </div>
+        <div className="sidebar" onClick={this.props.openSideBar}>
+          <CSSTransition
+            in={open}
+            classNames="slide-in"
+            unmountOnExit
+            timeout={500}>
+            <div className="sidebar-container">
+              <div className="location">
+                <DropDown />
               </div>
-            </CSSTransition>
-          </div>
-        </CSSTransition>
+              <div className="account">
+                <div className="account-icon" />
+              </div>
+              <div className="price-filter">
+                <div className="filter-icon" />
+              </div>
+              <div className="proximity-filter">
+                <div className="filter-icon" />
+              </div>
+              <div className="vegetarian-filter">
+                <div className="filter-icon" />
+              </div>
+            </div>
+          </CSSTransition>
+        </div>
       </>
     );
   }
