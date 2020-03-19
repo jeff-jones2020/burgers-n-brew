867c84dd54c57592a3ea99fb5efae1f644fbb977
Jeffery Jones <56287988+jeff-jones2020@users.noreply.github.com>
2020-03-19 15:50:39 -0700

    Add properties to deal components, added styling to match Figma

--- client/components/deals-feed.jsx
+++ client/components/deals-feed.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
 const DealsFeed = props => {
+  function setDetailView(e) {
+    props.setDetailViewCallback(e.id);
+  }
+
   const { restaurants } = props;
   const restaurantEls = restaurants.map((restaurant, index) => {
-    const flexReverser = index % 2 === 0 ? ' flex-row-reverse ' : '';
+    const flexReverser = index % 2 === 0 ? ' flex-row-reverse ' : ' ';
     return (
-      <div key={index} id={restaurant.id} className={'d-flex' + flexReverser + 'deal'}>
-        <div>
-          <img src={restaurant.photos[0]}/>
-        </div>
-        <div>
-          <h5>{restaurant.name}</h5>
-          <p></p>
+      <div
+        key={index}
+        id={restaurant.id}
+        onClick={setDetailView}
+        className={'d-flex' + flexReverser + 'align-items-center deal col-11 mx-auto p-0'}>
+        <img src={restaurant.photos[0]}/>
+        <div className='d-flex flex-column col'>
+          <h5 className='mb-0'>{restaurant.name}</h5>
+          <p>Spicy jalapeno bacon ipsum dolor amet dolor chislic lager.</p>
         </div>
       </div>
     );
