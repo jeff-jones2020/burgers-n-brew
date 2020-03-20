a3e6b53ed5e9706da200581825d64551d403c75a
Lawrence <lawrey7@gmail.com>
2020-03-20 12:41:30 -0700

    Added a DetailedView component.

---
+++ client/components/detail-view.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class DetailedView extends Component {
+  render() {
+    return (
+      <div>
+        <div className="carousel">
+          {/* image */}
+        </div>
+        <div>
+          <div>
+            {/* Restaurant Tag Headers */}
+          </div>
+          <div>
+            {/* Times Open/Closed */}
+          </div>
+          <div>
+            {/* Map? */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DetailedView;
