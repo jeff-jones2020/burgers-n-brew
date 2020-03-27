9f51430ca96cfa0171462deab5bbd23bd629a1c1
Jeffery Jones <56287988+jeff-jones2020@users.noreply.github.com>
2020-03-27 13:12:33 -0700

    Added endpoint to query sql database to post new reviews

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
 const app = express();
-const db = require('./lowdb');
+const lowdb = require('./lowdb');
 const fetch = require('node-fetch');
 const bcrypt = require('bcrypt');
 
@@ -12,19 +12,26 @@ app.use(staticMiddleware);
 app.use(sessionMiddleware);
 app.use(express.json());
 
+// For PostgreSQL ********
+const pg = require('pg');
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:lfz@localhost/bnb'
+});
+// For PostgreSQL ********
+
 const passport = require('./passport')(app);
 
 app.post('/api/signup', (req, res) => {
   const { email, pwd, name, city } = req.body;
   bcrypt.hash(pwd, 10, (err, hash) => {
-    const duplication = db
+    const duplication = lowdb
       .get('user')
       .find({ email })
       .value();
     if (duplication) {
       res.json({ err: 'Email already exist here' });
     } else {
-      const users = db.get('user').value();
+      const users = lowdb.get('user').value();
       const user = {
         id: users.length + 1,
         name: name.toLowerCase(),
@@ -32,7 +39,7 @@ app.post('/api/signup', (req, res) => {
         email: email.toLowerCase(),
         password: hash
       };
-      db.get('user')
+      lowdb.get('user')
         .push(user)
         .write();
       res.json([user, true]);
@@ -53,7 +60,7 @@ app.get('/api/user', (req, res) => {
 
 app.get('/api/home/user', (req, res) => {
   if (req.session.passport.user === req.user[0].email) {
-    const data = db.get('user').value();
+    const data = lowdb.get('user').value();
     const newUser = data.filter((user, i) => {
       return user.name === req.session.name;
     });
@@ -67,11 +74,11 @@ app.put('/api/home/user/:id', (req, res) => {
   const id = Number(req.params.id);
   const city = req.body.city.toLowerCase();
   if (req.session.passport.user === req.user[0].email) {
-    db.get('user')
+    lowdb.get('user')
       .find({ id })
       .assign({ city })
       .write();
-    const data = db
+    const data = lowdb
       .get('user')
       .find({ id })
       .value();
@@ -108,6 +115,44 @@ app.get('/api/yelp/businesses/:id', (req, res) => {
     .then(data => {
       res.status(200).json(data);
     });
+});
+
+app.post('/api/reviews', (req, res) => {
+  const { body } = req;
+  const { yelpId, reviewText } = body;
+  const userId = parseInt(body.userId, 10);
+  const rating = parseFloat(body.rating);
+  if (!userId || userId < 1 || (userId !== parseFloat(body.userId))) {
+    res.status(400).json({ message: 'Invalid userId: userId must be a positive integer' });
+    return;
+  } else if (!rating || rating < 1 || rating > 5) {
+    res.status(400).json({ message: 'Invalid rating: rating must be a positive decimal between 1 and 5' });
+    return;
+  }
+
+  const params = [userId, yelpId, rating, reviewText];
+  const sql = `
+    INSERT INTO reviews (user_id, yelp_id, rating, review_text)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
+  `;
+
+  db.query(sql, params)
+    .then(result => {
+      const review = result.rows[0];
+      if (!review) {
+        res.status(500).json({ message: 'Nothing returned from psql' });
+
+      } else {
+        res.json(review);
+
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    });
+
 });
 
 app.listen(process.env.PORT, () => {});
